573ac7681f9f29df2b166f50f44ac5762e450ef3
JACL <canalesdev@gmail.com>
2017-09-09 02:37:17 -0500

    archivos01
    archivos nuevos

---
+++ guardar.php
@@ -0,0 +1,30 @@
+<?php
+//conectamos Con el servidor
+$conectar=@mysql_connect('localhost','root','');
+//verificamos la conexion
+if(!$conectar){
+echo"No Se Pudo Conectar Con El Servidor";
+}else{
+
+$base=mysql_select_db('prueba');
+if(!$base){
+echo"No Se Encontro La Base De Datos";
+}
+}
+//recuperar las variables
+$dni=$_POST['dni'];
+$nombre=$_POST['nombre'];
+$apellido=$_POST['apellido'];
+//hacemos la sentencia de sql
+$sql="INSERT INTO datos VALUES('$dni',
+'$nombre',
+'$apellido')";
+//ejecutamos la sentencia de sql
+$ejecutar=mysql_query($sql);
+//verificamos la ejecucion
+if(!$ejecutar){
+echo"Hubo Algun Error";
+}else{
+echo"Datos Guardados Correctamente<br><a href='index1.html'>Volver</a>";
+}
+?>
